6d5d7ccf0d69971e1b9eb3491bbf9efe4089b3d9
demlsp2040 <demlsp2040@gmail.com>
2023-02-09 09:41:33 -0500

    added pyspark code

--- hadoop/pyspark/create_dataframe_from_s3.py
+++ hadoop/pyspark/create_dataframe_from_s3.py
@@ -26,7 +26,23 @@ df1=df.filter("account_id_type = 'Saving'")
 df2=df1.withColumn('min_balance',psf.lit(20))
 df3=df.filter("account_id_type != 'Saving'")
 df4=df3.withColumn('min_balance',psf.lit(10))
-df5=df4.unionAll(df3)
+df5=df4.unionAll(df2)
+
 
 
 # --spark-submit  create.py
+# df=spark.sql(""" select * from schema.table""")
+df5.coalesce(5).write.mode('overwrite').parquet("s3://aws-train-nov-de-data/data/src_customer/customer_details_parquet_write/")
+df5.repartition('account_id').write.mode('overwrite').parquet("s3://aws-train-nov-de-data/data/src_customer/customer_details_parquet_write/")
+
+spark.sql(""" inser overwrite table select a, b, case when () as d end from table 
+  inner join aaaa""")
+
+df=spark.read.parquet("s3://aws-train-nov-de-data/data/src_customer/customer_details_parquet/")
+df.count()
+
+spark.sql(""" select count(1) from schema.table """)
+
+# Service Principal: glue.amazonaws.com is not authorized to perform:
+# logs:PutLogEvents on resource: arn:aws:logs:us-east-1:341966982503:log-group:/aws-glue/crawlers:log-stream:src_customer-customer_details because no identity-based policy allows the logs:PutLogEvents action (Service: AWSLogs; Status Code: 400; Error Code: AccessDeniedException; Request ID: 0ae85245-d16f-4e93-9bfc-24bc5eff20a0; Proxy: null). For more information,
+# see Setting up IAM Permissions in the Developer Guide (http://docs.aws.amazon.com/glue/latest/dg/getting-started-access.html).
